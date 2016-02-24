a632be46e597470b762c80ef2603cab0f9f0be68
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2016-02-24 10:42:06 +0100

    Pequeñose cambios en el Send And Receive básico

--- P1/tutorial/sendAndReceive/sendAndReceive.cpp
+++ P1/tutorial/sendAndReceive/sendAndReceive.cpp
@@ -11,15 +11,15 @@ int main(int argc, char *argv[])
     MPI_Comm_size(MPI_COMM_WORLD, &size); // Obtenemos el número total de procesos
     MPI_Comm_rank(MPI_COMM_WORLD, &rank); // Obtenemos el valor de nuestro identificador
 
-    if (rank == 0) { // El proceso 0 solo envía
+    if (rank == 0) { // El primer proceso solo envía
         // Envia mensaje
-        MPI_Send(&contador // referencia al vector de elementos a enviar
+        MPI_Send(&rank // referencia al vector de elementos a enviar
                 ,1 // tamaño del vector a enviar
                 ,MPI_INT // Tipo de dato que envias
                 ,rank + 1 // pid del proceso destino
                 ,0 // etiqueta
                 ,MPI_COMM_WORLD); //Comunicador por el que se manda
-    } else if (rank == size - 1) {
+    } else if (rank == size - 1) { // El último proceso solo recibe
         // Recibe mensaje
         MPI_Recv(&contador // Referencia al vector donde se almacenara lo recibido
                 ,1 // tamaño del vector a recibir
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
                 ,MPI_COMM_WORLD // Comunicador por el que se recibe
                 ,&estado); // estructura informativa del estado
         cout << "Soy el proceso " << rank << " y he recibido " << contador << endl;
-    } else {
+    } else { // El resto de procesos reciben y envían
         // Recibe mensaje
         MPI_Recv(&contador // Referencia al vector donde se almacenara lo recibido
                 ,1 // tamaño del vector a recibir
@@ -41,7 +41,7 @@ int main(int argc, char *argv[])
         cout << "Soy el proceso " << rank << " y he recibido " << contador << endl;
         contador++;
         // Envia mensaje
-        MPI_Send(&contador // referencia al vector de elementos a enviar
+        MPI_Send(&rank // referencia al vector de elementos a enviar
                 ,1 // tamaño del vector a enviar
                 ,MPI_INT // Tipo de dato que envias
                 ,rank + 1 // pid del proceso destino
