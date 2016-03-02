cb093f252ebedfbbefd7464541a0d1e0632ee260
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2016-03-02 10:29:22 +0100

    Ejercicio básico de comunicadores con impresión desordenada

--- P1/tutorial/comunicadores/comunicadores.cpp
+++ P1/tutorial/comunicadores/comunicadores.cpp
@@ -24,13 +24,15 @@ int main(int argc, char *argv[]) {
         b = 0;
     }
 
-    int color = rank % 2;
+    int color = rank % 2; // color para saber si es de pares o de impares
 
+    // Se crean los comunicadores de pares e impares
     MPI_Comm_split(MPI_COMM_WORLD // Comunicador padre
         ,color // Identifica si par o impar
         ,rank // Prioridad para orden del rango en el nuevo comunicador
         ,&comm_par_impar); // Referencia al nuevo comunicador
 
+    // Se crea el comunicador de inversos
     MPI_Comm_split(MPI_COMM_WORLD // Comunicador padre
         ,0 // Identifica si par o impar
         ,-rank // Prioridad para orden del rango en el nuevo comunicador
@@ -42,10 +44,32 @@ int main(int argc, char *argv[]) {
     MPI_Comm_size(comm_inverso, &size_inverso); // Obtenemos el número total de procesos en el comunicador inverso
     MPI_Comm_rank(comm_inverso, &rank_inverso); // Obtenemos el valor de nuestro identificador en el comunicador inverso
 
-    cout << endl 
+    // Comprobar comunicadores
+    if (rank == 0) cout << "COMUNICADORES" << endl;
+    cout << endl
          << "Global: " << rank << endl 
-         << "Par/Impar: " << rank_par_impar << endl
+         << "Par/Impar: " << rank_par_impar << endl 
          << "Inverso: " << rank_inverso << endl;
+    // Difundir a solo a los pares
+    MPI_Bcast(&a // referencia al vector de elementos a enviar
+        ,1 // tamaño del vector a enviar
+        ,MPI_INT // tipo de dato que envia
+        ,0 // rango del proceso raiz
+        ,comm_par_impar); // Comunicador por el que se recibe
+
+    // Difundir b a todos
+    MPI_Bcast(&b // referencia al vector de elementos a enviar
+        ,1 // tamaño del vector a enviar
+        ,MPI_INT // tipo de dato que envia
+        ,size - 1 // rango del proceso raiz
+        ,comm_inverso); // Comunicador por el que se recibe
+
+    // Comprobar valores
+    if (rank == 0) cout << "VALORES" << endl;
+    cout << endl
+         << "Global: " << rank << endl
+         << "\tA: " << a << endl
+         << "\tB: " << b << endl;
 
     MPI_Finalize();
     return 0;
