ad2b492a7fd4a216f995fe4667d92645fef76b0d
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2016-03-09 10:32:04 +0100

    Reparto de tamaño del grafo y pasar local a global y viceversa

--- P1/version1D/Graph.h
+++ P1/version1D/Graph.h
@@ -8,16 +8,16 @@ const int INF= 100000;
 //**************************************************************************
 class Graph //Adjacency List clas
 {
-	private:
-	   int *A;
-	public:
-           Graph();
-           int vertices;
-	   void fija_nverts(const int verts);
-	   void inserta_arista(const int ptA,const int ptB, const int edge);
-	   int arista(const int ptA,const int ptB);
-           void imprime();
-           void lee(char *filename);
+private:
+    int *A;
+public:
+    Graph();
+    int vertices;
+    void fija_nverts(const int verts);
+    void inserta_arista(const int ptA,const int ptB, const int edge);
+    int arista(const int ptA,const int ptB);
+    void imprime();
+    void lee(char *filename);
 };
 
 //**************************************************************************
--- P1/version1D/floyd.cc
+++ P1/version1D/floyd.cc
@@ -6,48 +6,65 @@
 
 using namespace std;
 
-//**************************************************************************
+int pasarFilaALocal(int localSize, int globalIndex) {
+  return globalIndex % localSize;
+}
 
-int main (int argc, char *argv[])
+int pasarFilaAGlobal(int rank, int localSize, int localIndex) {
+  return rank * localSize + localIndex;
+}
+
+int main (int argc, char *argv[]) 
 {
-MPI::Init(argc,argv);
+  int rank, size, tama;
 
-if (argc != 2) 
-	{
-	 cerr << "Sintaxis: " << argv[0] << " <archivo de grafo>" << endl;
-	return(-1);
-	}
+  MPI_Init(&argc, &argv); // Inicializamos la comunicacion de los procesos
+  MPI_Comm_size(MPI_COMM_WORLD, &size); // Obtenemos el número total de procesos
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank); // Obtenemos el valor de nuestro identificador
 
-Graph G;
-G.lee(argv[1]);		// Read the Graph
-//cout << "EL Grafo de entrada es:"<<endl;
-//G.imprime();
-
-int nverts=G.vertices;
-
-double t=MPI::Wtime();
-// BUCLE PPAL DEL ALGORITMO
-int i,j,k,vikj;
-for(k=0;k<nverts;k++)
-  {
-   for(i=0;i<nverts;i++)
-     for(j=0;j<nverts;j++)
-      if (i!=j && i!=k && j!=k) 
-        {
-         vikj=G.arista(i,k)+G.arista(k,j);
-         vikj=min(vikj,G.arista(i,j));
-         G.inserta_arista(i,j,vikj);   
-        }
+  if (argc != 2) {
+    if (rank == 0) {
+      cerr << "Sintaxis: " << argv[0] << " <archivo de grafo>" << endl;
+    }
+    MPI_Finalize();
+    return -1;
   }
-  t=MPI::Wtime()-t;
-  MPI::Finalize();
 
+  Graph G;
+  int nverts;
+  if (rank == 0) {
+    G.lee(argv[1]);
+    //cout << "El Grafo de entrada es:" << endl;
+    //G.imprime();
+    nverts = G.vertices;
+  }
+
+  // Enviar el número de vértices a todos los procesos
+  MPI_Bcast(&nverts, 1, MPI_INT, 0, MPI_COMM_WORLD);
+  cout << "Proceso " << rank << " recibe tamaño " << nverts << endl;
+
+  double t = MPI_Wtime();
+  /*
+  // BUCLE PPAL DEL ALGORITMO
+  int i,j,k,vikj;
+  for (k = 0; k < nverts; k++) {
+    for (i = 0; i < nverts; i++) {
+      for (j = 0; j < nverts; j++) {
+        if (i != j && i != k && j != k) {
+          vikj = G.arista(i, k) + G.arista(k, j);
+          vikj = min(vikj, G.arista(i, j));
+          G.inserta_arista(i, j, vikj);   
+        }
+      }
+    }
+  }
+  */
+  t = MPI_Wtime() - t;
+  MPI_Finalize();
  
-  cout << endl<<"EL Grafo con las distancias de los caminos más cortos es:"<<endl<<endl;
-  G.imprime();
-  cout<< "Tiempo gastado= "<<t<<endl<<endl;
-
-
+  //cout << endl << "EL Grafo con las distancias de los caminos más cortos es:" << endl << endl;
+  //G.imprime();
+  cout << "Tiempo gastado= " << t<< endl << endl;
 }
 
 
