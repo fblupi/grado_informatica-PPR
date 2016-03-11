0f6829332f4309a6b7273632b8876ce3ea41952b
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2016-03-11 16:24:41 +0100

    Gather funcionando

--- P1/version1D/floyd.cc
+++ P1/version1D/floyd.cc
@@ -25,36 +25,52 @@ int main (int argc, char *argv[])
 {
   int rank, size, tama;
 
+  /**
+    * Paso 1: Iniciar MPI y obtener tamaño e id para cada proceso
+    */
   MPI_Init(&argc, &argv); // Inicializamos la comunicacion de los procesos
   MPI_Comm_size(MPI_COMM_WORLD, &size); // Obtenemos el número total de procesos
   MPI_Comm_rank(MPI_COMM_WORLD, &rank); // Obtenemos el valor de nuestro identificador
 
-  if (argc != 2) {
-    if (rank == 0) {
+  /**
+    * Paso 2: Comprobar entradas
+    */
+  if (argc != 2) { // Debe haber dos argumentos
+    if (rank == 0) { // El proceso 0 imprime el error
       cerr << "Sintaxis: " << argv[0] << " <archivo de grafo>" << endl;
     }
     MPI_Finalize();
     return -1;
   }
   
+  /**
+    * Paso 3: Crear grafo y obtener número de vértices
+    */
   Graph G;
-  int nverts, tamaMatriz, tamaBloque;
-  if (rank == 0) {
+  int nverts, tamaLocal, tamaBloque;
+  if (rank == 0) { // Solo lo hace un proceso
     G.lee(argv[1]);
     cout << "El Grafo de entrada es:" << endl;
     G.imprime();
     nverts = G.vertices;
   }
 
-  // Enviar el número de vértices a todos los procesos
+  /**
+    * Paso 4: Hacer broadcast del número de vértices a todos los procesos
+    */
   MPI_Bcast(&nverts, 1, MPI_INT, 0, MPI_COMM_WORLD);
-  cout << "Proceso " << rank << " recibe tamaño " << nverts << endl;
-  tamaMatriz = nverts * nverts / size;
 
-  int * M = new int [tamaMatriz], 
+  /**
+    * Paso 5: Reservar espacio para matriz y fila k
+    */
+  tamaLocal = nverts * nverts / size;
+  int * M = new int [tamaLocal], 
       * filaK = new int [nverts];
 
-  MPI_Scatter(G.ptrMatriz(), tamaMatriz, MPI_INT, M, tamaMatriz, MPI_INT, 0, MPI_COMM_WORLD);
+  /**
+    * Paso 6: Repartir matriz entre los procesos
+    */
+  MPI_Scatter(G.ptrMatriz(), tamaLocal, MPI_INT, M, tamaLocal, MPI_INT, 0, MPI_COMM_WORLD);
 
 /*
   double t = MPI_Wtime();
@@ -79,13 +95,21 @@ int main (int argc, char *argv[])
   }
 
   t = MPI_Wtime() - t;
+  */
+
+  /**
+    * Paso N - 1: Recoger resultados en la matriz
+    */
+  MPI_Gather(&M[0], tamaLocal, MPI_INT, G.ptrMatriz(), tamaLocal, MPI_INT, 0, MPI_COMM_WORLD);
+
+  /**
+    * Paso N: Finalizar e imprimir resultados
+    */
   MPI_Finalize();
  
   //cout << endl << "EL Grafo con las distancias de los caminos más cortos es:" << endl << endl;
   //G.imprime();
-  cout << "Tiempo gastado= " << t<< endl << endl;
-  */
-  MPI_Finalize();
+  //cout << "Tiempo gastado= " << t<< endl << endl;
 
 }
 
