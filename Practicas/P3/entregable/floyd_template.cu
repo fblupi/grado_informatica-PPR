8e191f69209d57396df234fda20f11d1377f3ae0
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2016-05-31 18:14:35 +0200

    Segunda versión del CUDA 2D Shared #18

--- Practicas/P3/entregable/floyd_template.cu
+++ Practicas/P3/entregable/floyd_template.cu
@@ -16,9 +16,9 @@ using namespace std;
 
 // Kernel 1D
 __global__ void floyd_kernel1D(int * M, const int nverts, const int k) {
-  int ij = blockIdx.x * blockDim.x + threadIdx.x,
-      i = ij / nverts,
-      j = ij - i * nverts;
+  int ij = blockIdx.x * blockDim.x + threadIdx.x, // celda ij en la matriz
+      i = ij / nverts,                            // índice i en la matriz
+      j = ij - i * nverts;                        // índice j en la matriz
 
   if (i < nverts && j < nverts) {
     if (i != j && i != k && j != k) {
@@ -29,8 +29,8 @@ __global__ void floyd_kernel1D(int * M, const int nverts, const int k) {
 
 // Kernel 2D
 __global__ void floyd_kernel2D(int * M, const int nverts, const int k) {
-  int i = blockIdx.y * blockDim.y + threadIdx.y,
-      j = blockIdx.x * blockDim.x + threadIdx.x,
+  int i = blockIdx.y * blockDim.y + threadIdx.y,  // índice i en la matriz
+      j = blockIdx.x * blockDim.x + threadIdx.x,  // índice j en la matriz
       ij;
 
   if (i < nverts && j < nverts) {
@@ -43,7 +43,7 @@ __global__ void floyd_kernel2D(int * M, const int nverts, const int k) {
 
 // Kernel Shared 1D
 __global__ void floyd_kernel1DShared(int * d_M, const int nverts, const int k) {
-  int blockPos = blockIdx.x * blockDim.x, // posicion inicial del bloque en memoria
+  int blockPos = blockIdx.x * blockDim.x, // posición inicial del bloque en memoria
       g_ij = blockPos + threadIdx.x,      // índice global de memoria == ij
       i = g_ij / nverts,                  // índice i en la matriz
       j = g_ij - i * nverts;              // índice j en la matriz
@@ -56,13 +56,13 @@ __global__ void floyd_kernel1DShared(int * d_M, const int nverts, const int k) {
                    s_Mk[BLOCK_SIZE_1D],
                    s_Mik[2];
 
-    s_Mi[l_ij] = d_M[g_ij];           // Copia la celda correspondiente a la fila i
-    s_Mk[l_ij] = d_M[k * nverts + j]; // Copia la celda correspondiente a la fila j
+    s_Mi[l_ij] = d_M[g_ij];           // copia la celda correspondiente a la fila i
+    s_Mk[l_ij] = d_M[k * nverts + j]; // copia la celda correspondiente a la fila j
 
     if (l_ij == 0) {
-      s_Mik[0] = d_M[i * nverts + k]; // Copia la celda (i, k)
+      s_Mik[0] = d_M[i * nverts + k]; // copia la celda (i, k)
     } else if(l_ij == BLOCK_SIZE_1D - 1) {
-      s_Mik[1] = d_M[i * nverts + k]; // Copia la celda (i + 1, k)
+      s_Mik[1] = d_M[i * nverts + k]; // copia la celda (i + 1, k)
     }
 
     __syncthreads();
@@ -79,25 +79,62 @@ __global__ void floyd_kernel1DShared(int * d_M, const int nverts, const int k) {
 
 // Kernel Shared 2D
 __global__ void floyd_kernel2DShared(int * d_M, const int nverts, const int k) {
-  int i = blockIdx.y * blockDim.y + threadIdx.y,
-      j = blockIdx.x * blockDim.x + threadIdx.x,
+  int i = blockIdx.y * blockDim.y + threadIdx.y,  // índice i en la matriz
+      j = blockIdx.x * blockDim.x + threadIdx.x,  // índice j en la matriz
       ij, l_i, l_j;
 
   if (i < nverts && j < nverts) {
     __shared__ int s_filK[BLOCK_SIZE_2D],
                    s_colK[BLOCK_SIZE_2D];
 
-    l_i = threadIdx.y;
-    l_j = threadIdx.x;
+    l_i = threadIdx.y;  // índice i en la submatriz
+    l_j = threadIdx.x;  // índice j en la submatriz
 
-    if (l_i == 0) s_filK[l_j] = d_M[k * nverts + j];
-    if (l_j == 0) s_colK[l_i] = d_M[i * nverts + k];
+    if (l_i == 0) {                       // la primera fila
+      s_filK[l_j] = d_M[k * nverts + j];  // copia en la fila k
+    }
+    if (l_j == 0) {                       // la primera columna
+      s_colK[l_i] = d_M[i * nverts + k];  // copia en la columna k
+    }
 
     __syncthreads();
 
     if(i != j && i != k && j != k) {
       ij = i * nverts + j;
       d_M[ij] = min(s_colK[l_i] + s_filK[l_j], d_M[ij]);
+    }
+  }
+}
+
+// Kernel Shared 2D V2
+__global__ void floyd_kernel2DSharedV2(int * d_M, const int nverts, const int k) {
+  int i = blockIdx.y * blockDim.y + threadIdx.y,  // índice i en la matriz
+      j = blockIdx.x * blockDim.x + threadIdx.x,  // índice j en la matriz
+      ij, l_i, l_j, l_ij;
+
+  if (i < nverts && j < nverts) {
+    __shared__ int s_filK[BLOCK_SIZE_2D],
+                   s_colK[BLOCK_SIZE_2D],
+                   s_M[BLOCK_SIZE_2D * BLOCK_SIZE_2D];
+
+    l_i = threadIdx.y;                // índice i en la submatriz
+    l_j = threadIdx.x;                // índice i en la submatriz
+    l_ij = l_i * BLOCK_SIZE_2D + l_j; // celda ij en la submatriz
+    ij = i * nverts + j;              // celda ij en la matriz
+
+    s_M[l_ij] = d_M[ij];                  // todos copian en la submatriz compartida
+
+    if (l_i == 0) {                       // la primera fila
+      s_filK[l_j] = d_M[k * nverts + j];  // copia en la fila k
+    }
+    if (l_j == 0) {                       // la primera columna
+      s_colK[l_i] = d_M[i * nverts + k];  // copia en la columna k
+    }
+
+    __syncthreads();
+
+    if(i != j && i != k && j != k) {
+      d_M[ij] = min(s_colK[l_i] + s_filK[l_j], s_M[l_ij]);
     }
   }
 }
@@ -142,18 +179,20 @@ int main(int argc, char *argv[]) {
   const dim3 nblocks2D (ceil((float) nverts / blocksize2D.x),             // Num Bloques 2D
                         ceil((float) nverts / blocksize2D.y));
 
-  int * c_out_M_1D = new int[nverts2];        // Matriz en el HOST 1D
-  int * c_out_M_2D = new int[nverts2];        // Matriz en el HOST 2D
-  int * c_out_M_1DShared = new int[nverts2];  // Matriz en el HOST 1D Shared
-  int * c_out_M_2DShared = new int[nverts2];  // Matriz en el HOST 2D Shared
-  int size = nverts2 * sizeof(int);           // Tama en bytes de la matriz de salida
-  int * d_In_M_1D = NULL;                     // Matriz en DEVICE para 1D
-  int * d_In_M_2D = NULL;                     // Matriz en DEVICE para 2D
-  int * d_In_M_1DShared = NULL;               // Matriz en DEVICE para 1D Shared
-  int * d_In_M_2DShared = NULL;               // Matriz en DEVICE para 2D Shared
+  int * c_out_M_1D = new int[nverts2];          // Matriz en el HOST 1D
+  int * c_out_M_2D = new int[nverts2];          // Matriz en el HOST 2D
+  int * c_out_M_1DShared = new int[nverts2];    // Matriz en el HOST 1D Shared
+  int * c_out_M_2DShared = new int[nverts2];    // Matriz en el HOST 2D Shared
+  int * c_out_M_2DSharedV2 = new int[nverts2];  // Matriz en el HOST 2D Shared V2
+  int size = nverts2 * sizeof(int);             // Tama en bytes de la matriz de salida
+  int * d_In_M_1D = NULL;                       // Matriz en DEVICE para 1D
+  int * d_In_M_2D = NULL;                       // Matriz en DEVICE para 2D
+  int * d_In_M_1DShared = NULL;                 // Matriz en DEVICE para 1D Shared
+  int * d_In_M_2DShared = NULL;                 // Matriz en DEVICE para 2D Shared
+  int * d_In_M_2DSharedV2 = NULL;               // Matriz en DEVICE para 2D Shared V2
 
   int i, j, k;
-  double T, Tgpu1D, Tgpu2D, Tgpu1DShared, Tgpu2DShared, Tcpu;
+  double T, Tgpu1D, Tgpu2D, Tgpu1DShared, Tgpu2DShared, Tgpu2DSharedV2, Tcpu;
 
   //************************************************************************************************
   // GPU phase (1D)
@@ -291,6 +330,40 @@ int main(int argc, char *argv[]) {
   Tgpu2DShared = (Tgpu2DShared - T) / CLOCKS_PER_SEC;
   cout << "Tiempo gastado GPU (2D Shared) = " << Tgpu2DShared << endl;
 
+  //************************************************************************************************
+  // GPU phase (2D Shared Memory V2)
+
+  // Reservar espacio en memoria para la matriz en DEVICE
+  err = cudaMalloc((void **) &d_In_M_2DSharedV2, size);
+  if (err != cudaSuccess) {
+    cout << "ERROR: Bad Allocation in Device Memory" << endl;
+  }
+
+  T = clock();
+
+  // Copiar los datos de la matriz en HOST en la matriz en DEVICE
+  err = cudaMemcpy(d_In_M_2DSharedV2, G.Get_Matrix(), size, cudaMemcpyHostToDevice);
+  if (err != cudaSuccess) {
+    cout << "ERROR: COPY MATRIX TO DEVICE" << endl;
+  }
+
+  for (k = 0; k < niters; k++) {
+    // Kernel Launch
+    floyd_kernel2DSharedV2 <<< nblocks2D, blocksize2D >>> (d_In_M_2DSharedV2, nverts, k);
+    err = cudaGetLastError();
+    if (err != cudaSuccess) {
+      fprintf(stderr, "Failed to launch kernel!\n");
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  // Copiar los datos de la matriz en DEVICE en la matriz en HOST
+  cudaMemcpy(c_out_M_2DSharedV2, d_In_M_2DSharedV2, size, cudaMemcpyDeviceToHost);
+
+  Tgpu2DSharedV2 = clock();
+  Tgpu2DSharedV2 = (Tgpu2DSharedV2 - T) / CLOCKS_PER_SEC;
+  cout << "Tiempo gastado GPU (2D Shared V2) = " << Tgpu2DSharedV2 << endl;
+
 #ifndef TIEMPOS
   //************************************************************************************************
   // CPU phase
@@ -318,6 +391,7 @@ int main(int argc, char *argv[]) {
   cout << "Ganancia (2D) = " << Tcpu / Tgpu2D << endl;
   cout << "Ganancia (1D Shared) = " << Tcpu / Tgpu1DShared << endl;
   cout << "Ganancia (2D Shared) = " << Tcpu / Tgpu2DShared << endl;
+  cout << "Ganancia (2D Shared V2) = " << Tcpu / Tgpu2DSharedV2 << endl;
 
   //************************************************************************************************
   // Comprobar que los resultados en CPU y GPU son los mismos
@@ -340,6 +414,10 @@ int main(int argc, char *argv[]) {
         cout << "Error 2D Shared (" << i << "," << j << ")   " << c_out_M_2DShared[i * nverts + j]
              << "..." << G.arista(i, j) << endl;
       }
+      if (abs(c_out_M_2DSharedV2[i * nverts + j] - G.arista(i, j)) > 0) {
+        cout << "Error 2D Shared V2 (" << i << "," << j << ")   " << c_out_M_2DSharedV2[i * nverts + j]
+             << "..." << G.arista(i, j) << endl;
+      }
     }
   }
   //************************************************************************************************
@@ -350,8 +428,10 @@ int main(int argc, char *argv[]) {
   cudaFree(d_In_M_2D);
   cudaFree(d_In_M_1DShared);
   cudaFree(d_In_M_2DShared);
+  cudaFree(d_In_M_2DSharedV2);
   delete[] c_out_M_1D;
   delete[] c_out_M_2D;
   delete[] c_out_M_1DShared;
   delete[] c_out_M_2DShared;
+  delete[] c_out_M_2DSharedV2;
 }
---
+++ Practicas/P3/entregable/readme.txt
@@ -0,0 +1,2 @@
+La segunda versión de la versión con memoria compartida en 2D no funciona con tamaños muy grandes
+pues usa el stack para almacenar la submatriz.
